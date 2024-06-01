daea9e8fcf996b9114a779b3d993c0e99b9fb4a9
Ricardo A. F. da Cruz <ricardofrancocruz@gmail.com>
2024-05-31 23:19:47 -0300

    Adicionado Client, Account, Transaction e CreateClientUseCase

---
+++ internal/entity/account_test.go
@@ -0,0 +1,30 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	assert.NotNil(t, account)
+	assert.Equal(t, client.ID, account.Client.ID)
+}
+
+func TestCreaditAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	account.Credit(100)
+	assert.Equal(t, float64(100), account.Balance)
+}
+
+func TestDebitAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	account.Credit(100)
+	account.Debit(50)
+	assert.Equal(t, float64(50), account.Balance)
+
+}
